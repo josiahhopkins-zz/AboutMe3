ad30f18d2710ecd611676507f1503c0752f0db92
josiahhopkins <hopkinsjosiah@gmail.com>
2017-04-07 18:25:33 -0700

    Testing launching

--- ImageActivity.java
+++ ImageActivity.java
@@ -3,11 +3,11 @@ package a450.tcss.uw.edu.aboutme;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class TextActivity extends AppCompatActivity {
+public class ImageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_text);
+        setContentView(R.layout.activity_image);
     }
 }
